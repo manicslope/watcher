d91b0bc020673a28b2849fcc11e7dcdf2d7c5f7a
Your Name <you@example.com>
2025-02-03 20:50:16 +0100

    get photo every minute

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import os
 
 import requests
+from datetime import datetime
+import time
+import logging
+
+logging.basicConfig(format='%(asctime)s [%(levelname)-2s] %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
 camera_address = os.environ['camera_address']
 capture_url = 'http://' + '/'.join([camera_address, 'capture'])
+timeout = 60
+time_format = '%Y-%m-%dT%H%M%S'
 
-with open('capture.jpg', 'wb') as handler:
-    handler.write(requests.get(capture_url).content)
+
+def take_current_picture(filename):
+    with open(filename, 'wb') as handler:
+        handler.write(requests.get(capture_url).content)
+
+
+def watch():
+    while True:
+        logging.info("Taking picture...")
+        take_current_picture(datetime.today().strftime(time_format) + '.jpg')
+        time.sleep(timeout)
+
+
+if __name__ == "__main__":
+    watch()
